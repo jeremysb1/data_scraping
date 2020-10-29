e187fae298c2953267cf9b1a7a26b861427785ec
Jeremy Hall <jeremyhall@jeremys-mbp.attlocal.net>
2020-10-29 14:45:50 -0400

    adding updates

--- scrape.py
+++ scrape.py
@@ -3,9 +3,19 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+
 hn_text = BeautifulSoup(res.text, 'html.parser')
+hn_text2 = BeautifulSoup(res2.text, 'html.parser')
+
 links = hn_text.select('.storylink')
 subtext = hn_text.select('.subtext')
+
+links2 = hn_text2.select('.storylink')
+subtext2 = hn_text2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
 def sort_stories_by_votes(hnlist):
 	return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
@@ -25,4 +35,4 @@ def create_custom_hn(links, subtext):
 
 	return sort_stories_by_votes(hn)
 
-pprint.print(create_custom_hn, subtext)
+pprint.print(create_custom_hn(mega_links, mega_subtext))
